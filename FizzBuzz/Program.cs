426474d509daa7911c952c06002583fcf4425cdf
Beau Hansen <beau.hansen45@gmail.com>
2018-11-28 15:29:17 -0700

    commit 1. submitting incomplete assignment

---
+++ FizzBuzz/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace FizzBuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (var i = 1; i <= 1000; i++)
+            {
+                if ((i % 3) == 0) & ((i % 5 == 0));
+                {
+                    Console.WriteLine("{i} FizzBuzz");
+                }
+                else if ((i % 3) == 0) & ((i % 5 != 0));
+                {
+                    Console.WriteLine("{i} Fizz");
+                }
+                else if ((i % 3) != 0) & ((i % 5 == 0));
+                {
+                    Console.WriteLine("{i} Buzz");
+                }
+                else
+                {
+                    Console.WriteLine("{i}");
+                }
+            }
+            Console.WriteLine()
+        }
+    }
+}
