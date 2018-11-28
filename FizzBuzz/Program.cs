ab444b9460dddcb492112f4752976f3d341b191f
John Pot <j.pot@digipen.edu>
2018-11-28 16:21:05 -0600

    Completed FizzBuzz assignment - John Pot

---
+++ FizzBuzz/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace FizzBuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int fizzCount = 0;
+            int buzzCount = 0;
+            int fizzBuzzCount = 0;
+            for(int i = 0; i < 1000; i++)
+            {
+
+                if(i % 3 == 0 && i % 5 != 0)
+                {
+                    System.Console.WriteLine("Fizz");
+                    ++fizzCount;
+                }
+                else if(i % 5 == 0 && i % 3 != 0)
+                {
+                    System.Console.WriteLine("Buzz");
+                    ++buzzCount;
+                }
+                else if(i % 3 == 0 && i % 5 == 0)
+                {
+                    System.Console.WriteLine("FizzBuzz");
+                    ++fizzBuzzCount;
+                }
+                else
+                    System.Console.WriteLine(i);
+
+
+
+            }
+
+            System.Console.WriteLine("Total Fizz: " + fizzCount);
+            System.Console.WriteLine("Total Buzz: " + buzzCount);
+            System.Console.WriteLine("Total FizzBuzz: " + fizzBuzzCount);
+        }
+    }
+}
