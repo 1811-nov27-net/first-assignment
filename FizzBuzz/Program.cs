f296eec37536509442d8b1a6838818ad53a39ccf
justinzsy <justinzsy@gmail.com>
2018-11-28 16:27:41 -0600

    created console application in dotnet, which shows 'Fizz' for numbers divisible by 3 and not 5, 'Buzz' for numbers divisible by 5 and not 3, and 'Fizzbuzz' for numbers divisible by 3 and 5. Additionally it prints out the rest of the numbers that don't meet any of the criteria, and counts how many Fizz, Buzz, and Fizzbuzz there are

---
+++ FizzBuzz/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace FizzBuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int fizzCounter = 0;
+            int buzzCounter = 0;
+            int fizzbuzzCounter = 0;
+
+            for (double i = 1; i <= 1000; i++) {
+                if ((i/3) % 1 == 0 && (i/5) % 1 == 0) {
+                    Console.WriteLine("Fizzbuzz");
+                    fizzbuzzCounter++;
+                }
+                else if ((i/3) % 1 == 0 && (i/5) % 1 != 0) {
+                    Console.WriteLine("Fizz");
+                    fizzCounter++;
+                }
+                else if ((i/3) % 1 != 0 && (i/5) % 1 == 0) {
+                    Console.WriteLine("Buzz");
+                    buzzCounter++;
+                }
+                else{
+                    Console.WriteLine(i);
+                }
+            }
+
+            Console.WriteLine("Fizz: " + fizzCounter);
+            Console.WriteLine("Buzz: " + buzzCounter);
+            Console.WriteLine("Fizzbuzz: " + fizzbuzzCounter);
+        }
+    }
+}
