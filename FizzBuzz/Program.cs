5240dab99fc7c9f4e9b903f19619c5e6759aaea7
willbelt <willbeltit@gmail.com>
2018-11-28 16:22:40 -0600

    FizzBuzz completed

---
+++ FizzBuzz/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace FizzBuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i < 1001; i++)
+            {
+                if (i%3 == 0 && i%5 != 0)
+                    Console.WriteLine(i+". Fizz");
+                else if (i%3 != 0 && i%5 == 0)
+                    Console.WriteLine(i+". Buzz");
+                else if (i%3 == 0 && i%5 == 0)
+                    Console.WriteLine(i+". FizzBuzz");
+            }
+        }
+    }
+}
