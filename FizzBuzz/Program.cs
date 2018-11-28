dfebe0c56e176bc873316b7ea3df90630746b235
devin-rouse <devin.a.rouse@gmail.com>
2018-11-28 16:22:09 -0600

    Updated with completed FizzBuzz assignment

---
+++ FizzBuzz/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace FizzBuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Declare counter variables
+            int fizzCount = 0;
+            int buzzCount = 0;
+            int fizzbuzzCount = 0;
+
+            // For loop to count through the numbers
+            for(var i = 1; i <= 1000; i++) 
+            {
+                // If the number is divisible by BOTH 3 and 5
+                if (i%3 == 0 && i%5 == 0)
+                {
+                    System.Console.WriteLine("Fizzbuzz");
+                    fizzbuzzCount++;
+                }
+
+                // If the number is divisible by 3 and NOT 5
+                if (i%3 == 0 && i%5 != 0)
+                {
+                    System.Console.WriteLine("Fizz");
+                    fizzCount++;
+                }
+
+                // If the number is divisible by 5 and NOT 3
+                if (i%3 != 0 && i%5 == 0)
+                {
+                    System.Console.WriteLine("Buzz");
+                    buzzCount++;
+                }
+
+                // If the number is divisible by NEITHER 3 or 5
+                if (i%3 != 0 && i%5 != 0)
+                {
+                    System.Console.WriteLine(i);
+                    
+                }
+            }
+        }
+    }
+}
