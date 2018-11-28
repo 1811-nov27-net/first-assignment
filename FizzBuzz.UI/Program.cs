04ffef9dfa90ac0f465e97ab5822c2964e66a079
Howard Tran <howardlt94@gmail.com>
2018-11-28 16:35:29 -0600

    sasd

--- FizzBuzz.UI/Program.cs
+++ FizzBuzz.UI/Program.cs
@@ -7,6 +7,19 @@ namespace FizzBuzz.UI
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            for (var i = 0; i < 1000; i++)
+            {
+                ConsoleWriteLine(i);
+            }
+            while (false)
+            {
+                ConsoleWriteLine("FIZZ");
+            }
+            if (true)
+            {
+                ConsoleWriteLine("BUzz");
+            }
         }
     }
 }
