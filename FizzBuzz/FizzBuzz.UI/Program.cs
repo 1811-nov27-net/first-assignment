89fd1eed33d38d228a03fd1a2fce469f24d92225
Vishal Bhupathiraju <vishal.bhupathiraju@gmail.com>
2018-11-28 16:29:04 -0600

    second commit

--- FizzBuzz/FizzBuzz.UI/Program.cs
+++ FizzBuzz/FizzBuzz.UI/Program.cs
@@ -7,24 +7,34 @@ namespace FizzBuzz.UI
         static void Main(string[] args)
         {
             //Console.WriteLine("Hello World!");
+            int fizzbuzz = 0;
+            int fizz = 0;
+            int buzz = 0;
             for(int i = 1; i < 1000; i++)
             {
                 if((i%3==0)&&(i%5==0))
                 {
+                    fizzbuzz++;
                     Console.WriteLine("FizzBuzz");
                 }
                 else if(i%3==0)
                 {
+                    fizz++;
                     Console.WriteLine("Fizz");
                 }
                 else if(i%5==0)
                 {
+                    buzz++;
                     Console.WriteLine("Buzz");
                 }
                 else
                 {
                     Console.WriteLine(i);
                 }
+                Console.WriteLine("Fizz: " + fizz);
+                Console.WriteLine("Buzz: " + buzz);
+                Console.WriteLine("FizzBuzz: " + fizzbuzz);
+                
             }
         }
     }
