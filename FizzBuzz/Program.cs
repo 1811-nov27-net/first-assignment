97103145194f1fc39904f136b193cf995b80d4ea
Rogerio Pereira <rogerio@colmeiatecnologia.com.br>
2018-11-28 16:27:12 -0600

    FizzBuzz

---
+++ FizzBuzz/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace FizzBuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int _fizz = 0;
+            int _buzz = 0;
+            int _fizzBuzz = 0;
+            
+            for(var i=0; i<1000; i++)
+            {
+                if((i%3 == 0) && (i%5 != 0))
+                {
+                    Console.Write("Fizz");
+                    _fizz++;
+                }
+                else if((i%5 == 0) && (i%3 != 0))
+                {
+                    Console.Write("Buzz");
+                    _buzz++;
+                }
+                else if((i%5 == 0) && (i%3 == 0))
+                {
+                    Console.Write("FizzBuzz");
+                    _fizzBuzz++;
+                }
+                else
+                {
+                    Console.WriteLine(i);
+                }
+            }
+
+            Console.Write($"fizz: {_fizz}, buzz: {_buzz}, fizzbuzz: {_fizzBuzz}");
+        }
+    }
+}
